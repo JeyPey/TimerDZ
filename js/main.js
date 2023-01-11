6b58253cee12218f2eb19ac936a3f23ebf664dda
Vladimir <example@gmail.com>
2023-01-11 15:50:42 +0200

    add buttons

--- js/main.js
+++ js/main.js
@@ -10,13 +10,17 @@ let hour = 0,
     interval
 
 function startTimer() {
-    second++;
-    if (second >= 60) {
-        second = 0;
-        minute++;
-        if (minute >= 60) {
-            minute = 0;
-            hour++;
+    millisecond++;
+    if (millisecond >= 99) {
+        millisecond = 0;
+        second++;
+        if (second >= 60) {
+            second = 0;
+            minute++;
+            if (minute >= 60) {
+                minute = 0;
+                hour++;
+            }
         }
     }
 }
@@ -24,10 +28,26 @@ function Timer() {
     startTimer();
     time.textContent = (hour > 9 ? hour : "0" + hour)
         + ":" + (minute > 9 ? minute : "0" + minute)
-        + ":" + (second > 9 ? second : "0" + second);
+        + ":" + (second > 9 ? second : "0" + second)
+        + ":" + (millisecond > 9 ? millisecond : "0" + millisecond)
 }
 
+
+// EventListener------------------------------------------------------
 buttonStart.addEventListener('click', () => {
     clearInterval(interval)
     interval = setInterval(Timer, 10)
+})
+
+buttonStop.addEventListener('click', () => {
+    clearInterval(interval)
+})
+
+buttonReset.addEventListener('click', () => {
+    clearInterval(interval)
+    time.textContent = "00:00:00:00"
+    hour = 0
+    minute = 0
+    second = 0
+    millisecond = 0
 })
