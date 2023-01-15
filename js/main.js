78a0807852953ba6658fe725a67b7511c9334b62
Vladimir <example@gmail.com>
2023-01-15 14:55:36 +0200

    Finally

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const time = document.querySelector('.time')
 const buttonStart = document.querySelector('.start')
 const buttonStop = document.querySelector('.stop')
 const buttonReset = document.querySelector('.reset')
+const buttonLap = document.querySelector('.lap')
 
 let hour = 0,
     minute = 0,
@@ -24,7 +25,7 @@ function startTimer() {
         }
     }
 }
-function Timer() {
+function timer() {
     startTimer();
     time.textContent = (hour > 9 ? hour : "0" + hour)
         + ":" + (minute > 9 ? minute : "0" + minute)
@@ -32,11 +33,19 @@ function Timer() {
         + ":" + (millisecond > 9 ? millisecond : "0" + millisecond)
 }
 
+function clearTime(){
+    time.textContent = "00:00:00:00"
+    hour = 0
+    minute = 0
+    second = 0
+    millisecond = 0
+}
+
 
 // EventListener------------------------------------------------------
 buttonStart.addEventListener('click', () => {
     clearInterval(interval)
-    interval = setInterval(Timer, 10)
+    interval = setInterval(timer, 10)
 })
 
 buttonStop.addEventListener('click', () => {
@@ -45,9 +54,17 @@ buttonStop.addEventListener('click', () => {
 
 buttonReset.addEventListener('click', () => {
     clearInterval(interval)
-    time.textContent = "00:00:00:00"
-    hour = 0
-    minute = 0
-    second = 0
-    millisecond = 0
+    clearTime()
 })
+
+buttonLap.addEventListener('click', () => {
+    clearInterval(interval)
+    const lapp = document.querySelector('.lapp')
+    const block = document.createElement('div')
+    block.innerText = `LAP: ${hour}:${minute}:${second}:${millisecond}`
+    lapp.append(block) 
+    clearTime()
+    clearInterval(interval)
+    interval = setInterval(timer, 10)
+})
+
